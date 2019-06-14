3a930fca4155c2f7bfb5bb8614181f2c00c8be5d
KingdevNL <jasper@kingdev.nl>
2019-06-14 22:08:10 +0200

    Image type check when loading

--- Engine/Texture.cpp
+++ Engine/Texture.cpp
@@ -6,16 +6,26 @@
 Texture::Texture(const char* file) {
 	glGenTextures(1, &textureID);
 	rgb = stbi_load(file, &width, &height, &bpp, 3);
-	if(!rgb) {
-		std::cerr << "Failed to load texture "<<file<<"\n";
+	if (!rgb) {
+		std::cerr << "Failed to load texture " << file << "\n";
 		exit(1);
 	}
-	
-	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, rgb);
-	
-	 glGenerateMipmap(GL_TEXTURE_2D);
 
+	
+
+	std::cout << bpp;
+	
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	if(bpp == 3) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, rgb);
+	} else if(bpp = 4) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgb);
+
+	}
+	
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 
 	std::cout << "Generated texture " << textureID << std::endl;
